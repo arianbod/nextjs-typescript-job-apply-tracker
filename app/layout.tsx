c6aad90f4e1fde4b686cb051b05e46d3339ba572
Arian <Arianbod@outlook.com>
2024-03-10 01:36:51 +0300

    fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import Providers from './providers';
 const inter = Inter({ subsets: ['latin'] });
+import Head from 'next/head';
 
-export const metadata: Metadata = {
-	title: 'Jobify Dev',
-	description: 'Job application tracking system for job hunters',
+// Inside your component
+export const viewport: Viewport = {
 	themeColor: [
 		{ media: '(prefers-color-scheme: light)', color: 'white' },
 		{ media: '(prefers-color-scheme: dark)', color: 'black' },
 	],
+};
+export const metadata: Metadata = {
+	title: 'Jobify Dev',
+	description: 'Job application tracking system for job hunters',
 };
 
 export default function RootLayout({
