400487ffe66a827f15ddb6e885d5a187c0107037
Arian <Arianbod@outlook.com>
2024-03-11 01:55:55 +0300

    add react query

--- app/(pages)/(dashboard)/jobs/page.tsx
+++ app/(pages)/(dashboard)/jobs/page.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {
+	dehydrate,
+	HydrationBoundary,
+	QueryClient,
+} from '@tanstack/react-query';
+import JobsList from '@/components/jobs/JobsList';
+import SearchForm from '@/components/jobs/SearchForm';
+import { getAllJobsAction } from '@/utils/actions';
 
-const JobPage = () => {
-	return <div className='text-7xl'>JobPage</div>;
+const JobPage = async () => {
+	const queryClient = new QueryClient();
+	await queryClient.prefetchQuery({
+		queryKey: ['jobs', '', 'all', 1],
+		queryFn: () => getAllJobsAction({}),
+	});
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<SearchForm />
+			<JobsList />
+		</HydrationBoundary>
+	);
 };
 
 export default JobPage;
